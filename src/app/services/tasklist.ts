c7b4e9ffbb67b84c0212c3b407ad1ca835902884
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-08-11 14:20:44 -0300

    tareas de prueba

--- src/app/services/tasklist.ts
+++ src/app/services/tasklist.ts
@@ -1,11 +1,9 @@
 import { Task } from "./task";
 
 export const Tasklist: Task[] = [
-    { id: 1, text: "Tarea de Prueba 1", day: new Date(), reminder: false },
-    { id: 2, text: "Tarea de Prueba 2", day: new Date(), reminder: false },
-    { id: 3, text: "Tarea de Prueba 3", day: new Date(), reminder: false },
-    { id: 4, text: "Tarea de Prueba 4", day: new Date(), reminder: false },
-    { id: 5, text: "Tarea de Prueba 5", day: new Date(), reminder: false },
-    { id: 6, text: "Tarea de Prueba 6", day: new Date(), reminder: false },
-    { id: 7, text: "Tarea de Prueba 7", day: new Date(), reminder: false }
+    { id: 1, text: "Tarea de Prueba 1", day: new Date('2022-08-11T13:10'), reminder: false },
+    { id: 2, text: "Tarea de Prueba 2", day: new Date('2022-08-11T14:14'), reminder: true },
+    { id: 3, text: "Tarea de Prueba 3", day: new Date('2022-08-11T15:25'), reminder: false },
+    { id: 4, text: "Tarea de Prueba 4", day: new Date('2022-08-12T08:14'), reminder: true },
+    { id: 5, text: "Tarea de Prueba 5", day: new Date('2022-08-13T21:30'), reminder: false }
 ];
